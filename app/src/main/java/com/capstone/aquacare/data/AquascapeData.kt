8a58655aa3f36a18903682c3074add4c00e8d328
Niki Muhammad Nafis <nikinafis08@gmail.com>
2024-03-24 23:34:04 +0700

    Add AquascapeData.kt

---
+++ app/src/main/java/com/capstone/aquacare/data/AquascapeData.kt
@@ -0,0 +1,8 @@
+package com.capstone.aquacare.data
+
+data class AquascapeData(
+    val id: String? = null,
+    val name: String? = null,
+    val style: String? = null,
+    val date: String? = null
+)
